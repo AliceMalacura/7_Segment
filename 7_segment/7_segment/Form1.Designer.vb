0f62ea1accac0c9ec8733b4a79a7516b78817aa7
Alice Jane Malacura <35315202+AliceMalacura@users.noreply.github.com>
2020-03-13 10:37:17 +0800

    Add files via upload

---
+++ 7_segment/7_segment/Form1.Designer.vb
@@ -0,0 +1,316 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.display_f = New System.Windows.Forms.PictureBox()
+        Me.display_b = New System.Windows.Forms.PictureBox()
+        Me.display_a = New System.Windows.Forms.PictureBox()
+        Me.display_g = New System.Windows.Forms.PictureBox()
+        Me.display_d = New System.Windows.Forms.PictureBox()
+        Me.display_c = New System.Windows.Forms.PictureBox()
+        Me.display_e = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.rb1 = New System.Windows.Forms.RadioButton()
+        Me.rb2 = New System.Windows.Forms.RadioButton()
+        Me.rb3 = New System.Windows.Forms.RadioButton()
+        Me.rb6 = New System.Windows.Forms.RadioButton()
+        Me.rb5 = New System.Windows.Forms.RadioButton()
+        Me.rb4 = New System.Windows.Forms.RadioButton()
+        Me.rb9 = New System.Windows.Forms.RadioButton()
+        Me.rb8 = New System.Windows.Forms.RadioButton()
+        Me.rb7 = New System.Windows.Forms.RadioButton()
+        Me.rb0 = New System.Windows.Forms.RadioButton()
+        CType(Me.display_f, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.display_b, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.display_a, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.display_g, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.display_d, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.display_c, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.display_e, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'display_f
+        '
+        Me.display_f.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.display_f.Location = New System.Drawing.Point(40, 75)
+        Me.display_f.Name = "display_f"
+        Me.display_f.Size = New System.Drawing.Size(26, 86)
+        Me.display_f.TabIndex = 0
+        Me.display_f.TabStop = False
+        '
+        'display_b
+        '
+        Me.display_b.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.display_b.Location = New System.Drawing.Point(156, 75)
+        Me.display_b.Name = "display_b"
+        Me.display_b.Size = New System.Drawing.Size(26, 86)
+        Me.display_b.TabIndex = 1
+        Me.display_b.TabStop = False
+        '
+        'display_a
+        '
+        Me.display_a.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.display_a.Location = New System.Drawing.Point(72, 47)
+        Me.display_a.Name = "display_a"
+        Me.display_a.Size = New System.Drawing.Size(78, 27)
+        Me.display_a.TabIndex = 2
+        Me.display_a.TabStop = False
+        '
+        'display_g
+        '
+        Me.display_g.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.display_g.Location = New System.Drawing.Point(72, 156)
+        Me.display_g.Name = "display_g"
+        Me.display_g.Size = New System.Drawing.Size(78, 27)
+        Me.display_g.TabIndex = 3
+        Me.display_g.TabStop = False
+        '
+        'display_d
+        '
+        Me.display_d.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.display_d.Location = New System.Drawing.Point(72, 273)
+        Me.display_d.Name = "display_d"
+        Me.display_d.Size = New System.Drawing.Size(78, 27)
+        Me.display_d.TabIndex = 7
+        Me.display_d.TabStop = False
+        '
+        'display_c
+        '
+        Me.display_c.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.display_c.Location = New System.Drawing.Point(156, 188)
+        Me.display_c.Name = "display_c"
+        Me.display_c.Size = New System.Drawing.Size(26, 86)
+        Me.display_c.TabIndex = 5
+        Me.display_c.TabStop = False
+        '
+        'display_e
+        '
+        Me.display_e.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.display_e.Location = New System.Drawing.Point(40, 188)
+        Me.display_e.Name = "display_e"
+        Me.display_e.Size = New System.Drawing.Size(26, 86)
+        Me.display_e.TabIndex = 4
+        Me.display_e.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Label1.Font = New System.Drawing.Font("Bernard MT Condensed", 20.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(208, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 31)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "7 SEGMENT"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Button1.Font = New System.Drawing.Font("Lucida Fax", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(218, 246)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(103, 41)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Click Here"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'rb1
+        '
+        Me.rb1.AutoSize = True
+        Me.rb1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb1.Location = New System.Drawing.Point(228, 146)
+        Me.rb1.Name = "rb1"
+        Me.rb1.Size = New System.Drawing.Size(31, 17)
+        Me.rb1.TabIndex = 10
+        Me.rb1.TabStop = True
+        Me.rb1.Text = "1"
+        Me.rb1.UseVisualStyleBackColor = False
+        '
+        'rb2
+        '
+        Me.rb2.AutoSize = True
+        Me.rb2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb2.Location = New System.Drawing.Point(228, 169)
+        Me.rb2.Name = "rb2"
+        Me.rb2.Size = New System.Drawing.Size(31, 17)
+        Me.rb2.TabIndex = 11
+        Me.rb2.TabStop = True
+        Me.rb2.Text = "2"
+        Me.rb2.UseVisualStyleBackColor = False
+        '
+        'rb3
+        '
+        Me.rb3.AutoSize = True
+        Me.rb3.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb3.Location = New System.Drawing.Point(228, 192)
+        Me.rb3.Name = "rb3"
+        Me.rb3.Size = New System.Drawing.Size(31, 17)
+        Me.rb3.TabIndex = 12
+        Me.rb3.TabStop = True
+        Me.rb3.Text = "3"
+        Me.rb3.UseVisualStyleBackColor = False
+        '
+        'rb6
+        '
+        Me.rb6.AutoSize = True
+        Me.rb6.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb6.Location = New System.Drawing.Point(275, 146)
+        Me.rb6.Name = "rb6"
+        Me.rb6.Size = New System.Drawing.Size(31, 17)
+        Me.rb6.TabIndex = 15
+        Me.rb6.TabStop = True
+        Me.rb6.Text = "6"
+        Me.rb6.UseVisualStyleBackColor = False
+        '
+        'rb5
+        '
+        Me.rb5.AutoSize = True
+        Me.rb5.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb5.Location = New System.Drawing.Point(275, 123)
+        Me.rb5.Name = "rb5"
+        Me.rb5.Size = New System.Drawing.Size(31, 17)
+        Me.rb5.TabIndex = 14
+        Me.rb5.TabStop = True
+        Me.rb5.Text = "5"
+        Me.rb5.UseVisualStyleBackColor = False
+        '
+        'rb4
+        '
+        Me.rb4.AutoSize = True
+        Me.rb4.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb4.Location = New System.Drawing.Point(228, 215)
+        Me.rb4.Name = "rb4"
+        Me.rb4.Size = New System.Drawing.Size(31, 17)
+        Me.rb4.TabIndex = 13
+        Me.rb4.TabStop = True
+        Me.rb4.Text = "4"
+        Me.rb4.UseVisualStyleBackColor = False
+        '
+        'rb9
+        '
+        Me.rb9.AutoSize = True
+        Me.rb9.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb9.Location = New System.Drawing.Point(275, 213)
+        Me.rb9.Name = "rb9"
+        Me.rb9.Size = New System.Drawing.Size(31, 17)
+        Me.rb9.TabIndex = 18
+        Me.rb9.TabStop = True
+        Me.rb9.Text = "9"
+        Me.rb9.UseVisualStyleBackColor = False
+        '
+        'rb8
+        '
+        Me.rb8.AutoSize = True
+        Me.rb8.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb8.Location = New System.Drawing.Point(275, 190)
+        Me.rb8.Name = "rb8"
+        Me.rb8.Size = New System.Drawing.Size(31, 17)
+        Me.rb8.TabIndex = 17
+        Me.rb8.TabStop = True
+        Me.rb8.Text = "8"
+        Me.rb8.UseVisualStyleBackColor = False
+        '
+        'rb7
+        '
+        Me.rb7.AutoSize = True
+        Me.rb7.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb7.Location = New System.Drawing.Point(275, 167)
+        Me.rb7.Name = "rb7"
+        Me.rb7.Size = New System.Drawing.Size(31, 17)
+        Me.rb7.TabIndex = 16
+        Me.rb7.TabStop = True
+        Me.rb7.Text = "7"
+        Me.rb7.UseVisualStyleBackColor = False
+        '
+        'rb0
+        '
+        Me.rb0.AutoSize = True
+        Me.rb0.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.rb0.Location = New System.Drawing.Point(228, 123)
+        Me.rb0.Name = "rb0"
+        Me.rb0.Size = New System.Drawing.Size(31, 17)
+        Me.rb0.TabIndex = 19
+        Me.rb0.TabStop = True
+        Me.rb0.Text = "0"
+        Me.rb0.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(358, 344)
+        Me.Controls.Add(Me.rb0)
+        Me.Controls.Add(Me.rb9)
+        Me.Controls.Add(Me.rb8)
+        Me.Controls.Add(Me.rb7)
+        Me.Controls.Add(Me.rb6)
+        Me.Controls.Add(Me.rb5)
+        Me.Controls.Add(Me.rb4)
+        Me.Controls.Add(Me.rb3)
+        Me.Controls.Add(Me.rb2)
+        Me.Controls.Add(Me.rb1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.display_d)
+        Me.Controls.Add(Me.display_c)
+        Me.Controls.Add(Me.display_e)
+        Me.Controls.Add(Me.display_g)
+        Me.Controls.Add(Me.display_a)
+        Me.Controls.Add(Me.display_b)
+        Me.Controls.Add(Me.display_f)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.display_f, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.display_b, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.display_a, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.display_g, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.display_d, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.display_c, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.display_e, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents display_f As PictureBox
+    Friend WithEvents display_b As PictureBox
+    Friend WithEvents display_a As PictureBox
+    Friend WithEvents display_g As PictureBox
+    Friend WithEvents display_d As PictureBox
+    Friend WithEvents display_c As PictureBox
+    Friend WithEvents display_e As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents rb1 As RadioButton
+    Friend WithEvents rb2 As RadioButton
+    Friend WithEvents rb3 As RadioButton
+    Friend WithEvents rb6 As RadioButton
+    Friend WithEvents rb5 As RadioButton
+    Friend WithEvents rb4 As RadioButton
+    Friend WithEvents rb9 As RadioButton
+    Friend WithEvents rb8 As RadioButton
+    Friend WithEvents rb7 As RadioButton
+    Friend WithEvents rb0 As RadioButton
+End Class
---
+++ 7_segment/7_segment/Form1.vb
@@ -0,0 +1,109 @@
+﻿Public Class Form1
+    Dim ButtonClickCount = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		ButtonClickCount += 1
+
+		If ButtonClickCount = 1 Then
+			display_a.BackColor = Color.Red
+			display_b.BackColor = Color.Red
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Red
+			display_e.BackColor = Color.Red
+			display_f.BackColor = Color.Red
+			display_g.BackColor = Color.Gray
+			rb0.Checked = True
+
+		ElseIf ButtonClickCount = 2 Then
+			display_a.BackColor = Color.Gray
+			display_b.BackColor = Color.Red
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Gray
+			display_e.BackColor = Color.Gray
+			display_f.BackColor = Color.Gray
+			display_g.BackColor = Color.Gray
+			rb1.Checked = True
+
+		ElseIf ButtonClickCount = 3 Then
+			display_a.BackColor = Color.Red
+			display_b.BackColor = Color.Red
+			display_c.BackColor = Color.Gray
+			display_d.BackColor = Color.Red
+			display_e.BackColor = Color.Red
+			display_f.BackColor = Color.Gray
+			display_g.BackColor = Color.Red
+			rb2.Checked = True
+
+		ElseIf ButtonClickCount = 4 Then
+			display_a.BackColor = Color.Red
+			display_b.BackColor = Color.Red
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Red
+			display_e.BackColor = Color.Gray
+			display_f.BackColor = Color.Gray
+			display_g.BackColor = Color.Red
+			rb3.Checked = True
+
+		ElseIf ButtonClickCount = 5 Then
+			display_a.BackColor = Color.Gray
+			display_b.BackColor = Color.Red
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Gray
+			display_e.BackColor = Color.Gray
+			display_f.BackColor = Color.Red
+			display_g.BackColor = Color.Red
+			rb4.Checked = True
+
+		ElseIf ButtonClickCount = 6 Then
+			display_a.BackColor = Color.Red
+			display_b.BackColor = Color.Gray
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Red
+			display_e.BackColor = Color.Gray
+			display_f.BackColor = Color.Red
+			display_g.BackColor = Color.Red
+			rb5.Checked = True
+
+		ElseIf ButtonClickCount = 7 Then
+			display_a.BackColor = Color.Red
+			display_b.BackColor = Color.Gray
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Red
+			display_e.BackColor = Color.Red
+			display_f.BackColor = Color.Red
+			display_g.BackColor = Color.Red
+			rb6.Checked = True
+
+		ElseIf ButtonClickCount = 8 Then
+			display_a.BackColor = Color.Red
+			display_b.BackColor = Color.Red
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Gray
+			display_e.BackColor = Color.Gray
+			display_f.BackColor = Color.Gray
+			display_g.BackColor = Color.Gray
+			rb7.Checked = True
+
+		ElseIf ButtonClickCount = 9 Then
+			display_a.BackColor = Color.Red
+			display_b.BackColor = Color.Red
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Red
+			display_e.BackColor = Color.Red
+			display_f.BackColor = Color.Red
+			display_g.BackColor = Color.Red
+			rb8.Checked = True
+
+		ElseIf ButtonClickCount = 10 Then
+			display_a.BackColor = Color.Red
+			display_b.BackColor = Color.Red
+			display_c.BackColor = Color.Red
+			display_d.BackColor = Color.Gray
+			display_e.BackColor = Color.Gray
+			display_f.BackColor = Color.Red
+			display_g.BackColor = Color.Red
+			rb9.Checked = True
+
+		End If
+	End Sub
+End Class
